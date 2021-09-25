49acc07aafdf18e5f6708dbd7d46cdcb78690b1c
soyeong <psoyeongg@gmail.com>
2021-09-25 22:45:53 +0900

    채우기 기능 추가

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = '#2c2c2c';
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
-
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
 }
 
 function startPainting() {
-    painting = true;
-}
+    if (!filling) {
+        painting = true;
+    }
+};
 
 function onMouseMove(event) {
     const x = event.offsetX;
@@ -30,8 +38,33 @@ function onMouseMove(event) {
         ctx.lineTo(x, y); // 선 끝 좌표
         ctx.stroke(); // 선 그리기
     }
+}
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
 
+function hangleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = '채우기';
+    } else {
+        filling = true;
+        mode.innerText = '그리기'
+    }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
 }
 
 if (canvas) {
@@ -39,13 +72,17 @@ if (canvas) {
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
-}
-
-function handleColorClick(event) {
-    const color = event.target.style.backgroundColor;
-    ctx.strokeStyle = color;
+    canvas.addEventListener('click', handleCanvasClick);
 }
 
 Array.from(colors).forEach(color => 
     color.addEventListener('click', handleColorClick)
 )
+
+if (range) {
+    range.addEventListener('input', hangleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener('click', handleModeClick);
+}
